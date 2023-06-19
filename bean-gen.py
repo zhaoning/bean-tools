1844d0f4326df13c9d70bc28b086bbc50593d802
Ning Zhao <zhaoning.zju@gmail.com>
2023-06-19 23:00:49 +1000

    Old script replaced

--- bean-gen.py
+++ bean-gen.py
@@ -1,252 +1,244 @@
 #!/usr/bin/env python3
+import pdb
+
 import os
 import re
 import sys
 import json
 import pathlib
-import argparse
 import itertools
 import subprocess
-
 from datetime import date, timedelta
+from argparse import ArgumentParser, Namespace
 
-parser = argparse.ArgumentParser(description='Beancount journal distributor')
+# Name of this script
+script_name = sys.argv[0]
 
-parser.add_argument('-f', '--files', nargs='*')
-parser.add_argument('-r', '--repo')
-parser.add_argument('-m', '--mono')
-parser.add_argument('-t', '--tree')
-parser.add_argument('-c', '--commit', action='store_true')
-parser.add_argument('-p', '--push', action='store_true')
-parser.add_argument('-v', '--verbose', action='count', default=0)
-parser.add_argument('-s', '--message', default='Committed by bean-gen')
+# Configurations
+conf = Namespace(
+        posting_indent=4,
+        metadata_indent=8,
+        decimal_align=50,
+        precision={'ETH': 6},
+        hashtag_chunk=re.compile(r'[0-9a-z]+')
+        )
 
-parser.add_argument('--posting-indent', default=4, type=int)
-parser.add_argument('--json-indent', default=4, type=int)
-parser.add_argument('--metadata-indent', default=8, type=int)
-parser.add_argument('--decimal-align', default=50, type=int)
+# Handle command line arguments
+ap = ArgumentParser(description='Beancount journal distributor')
+ap.add_argument('-f', '--files', nargs='*')
+ap.add_argument('-r', '--repo')
+ap.add_argument('-m', '--mono')
+ap.add_argument('-t', '--tree')
+ap.add_argument('-c', '--commit', action='store_true')
+ap.add_argument('-p', '--push', action='store_true')
+ap.add_argument('-v', '--verbose', action='count', default=0)
+ap.add_argument('-s', '--message', default=f'Committed by {script_name}')
+args = ap.parse_args()
 
-args = parser.parse_args()
+def format_hashtags(line, sep=' ', prefix='#', chunk=conf.hashtag_chunk):
+    """Re-format a string masking illegal characters.
 
-precision = {'ETH': 6}
-
-
-lowernums = re.compile(r'[0-9a-z]+')
-
-def mask_list(line, sep=' ', chunk=lowernums):
-    """Chop a string into a list and mask illegal characters.
-
-    Legal character is defined in regex `chunk`.  This function is created to
-    extract tags or links.
+    - All letters are converted to lowercase.
+    - Tags are separated by `sep`.
+    - Within each tag, chunks of legal characters are joined with '-'.
+    - Legal character is defined in regex `chunk`.
+    - Prefix of resulting hashtags can be customised through `prefix`.
+    - This function is created to extract tags or links.
     """
     ls = line.lower().split(sep)
     ls = ['-'.join(chunk.findall(s)) for s in ls]
 
-    return [s for s in ls if s]
+    return sep.join([f"{prefix}{s}" for s in ls if s])
 
-def tags(line):
-    """Format tags from comma separated string.
-    """
-    return ' '.join(['#' + s for s in mask_list(line)])
+class BeanMeta(Namespace):
+    def __init__(self, **data):
+        super().__init__(**data)
+        for k, v in self.__dict__.items():
+            if type(v) is str:
+                self.__dict__[k] = v.strip()
 
+    def __str__(self):
+        return '\n'.join([' ' * conf.metadata_indent
+                          + f"{k}: "
+                          + (f'"{v.strip()}"' if type(v) is str else str(v))
+                          for k, v in self.__dict__.items() if v])
 
-def links(line):
-    """Format links from comma separated string.
-    """
-    return ' '.join(['^' + s for s in mask_list(line)])
+class BeanOpen(Namespace):
+    def __init__(self, **data):
+        super().__init__()
+        self.date = data.pop('date', date.today().isoformat()).strip()
+        self.account = data.pop('account').strip()
+        self.currencies = data.pop('currencies', '').strip()
+        self.method = data.pop('method', '').strip()
+        self.meta = BeanMeta(**data)
 
-
-def metadata(adict):
-    meta = '\n'.join([' ' * args.metadata_indent + f"{k}: \"{str(v).strip()}\""
-                      for k, v in adict.items() if v])
-    return meta
-
-
-def posting(pdict, copy=True):
-    """Format posting from a posting dict.
-    """
-    pd = pdict.copy() if copy else pdict
-
-    account = pd.pop('account').strip()
-    amount = pd.pop('amount', 0)
-    currency = pd.pop('currency', '').strip()
-    cost = pd.pop('cost', '').strip()
-    price = pd.pop('price', '').strip()
-
-    line = ' ' * args.posting_indent + account
-
-    if amount:
-        p = precision.get(currency, 2)
-        amt_str = f"{amount:,.{p}f}"
-        try:
-            whole_amount_width = amt_str.index('.')
-        except ValuaError:
-            whole_amount_width = len(amt_str)
-        finally:
-            pad = ' ' * max(args.decimal_align
-                            - args.posting_indent
-                            - len(account)
-                            - whole_amount_width - 1, 2)
-        line += pad + amt_str
-
-        line += ' ' + currency if currency else ''
-        line += ' ' + cost if cost else ''
-        line += ' ' + price if price else ''
-
-    meta = metadata(pd)
-
-    if meta:
-        return line + '\n' + meta
-    else:
+    def __str__(self):
+        line = self.date + ' open ' + self.account
+        line += ' ' + self.currencies if self.currencies else ''
+        line += ' "' + self.method + '"' if self.method else ''
+        meta = str(self.meta)
+        line += '\n' + meta if meta else ''
         return line
 
+class BeanSchedule(Namespace):
+    def __init__(self, **data):
+        super().__init__(**data)
+        for k, v in self.__dict__.items():
+            if v and type(v) is str:
+                try:
+                    self.__dict__[k] = date.fromisoformat(v.strip())
+                except:
+                    pass
 
-def transaction(tdict):
-    """Format transaction line from transaction dict.
-    """
-    head = tdict.pop('date') + ' ' + tdict.pop('flag', '*')
+        if not hasattr(self, 'expire'):
+            self.expire = None
 
-    if 'payee' in tdict:
-        head += f" \"{tdict.pop('payee')}\""
+    def check(self, d):
+        """Check if date `d` is on the schedule.
 
-    head += f" \"{tdict.pop('narration', '')}\""
-
-    if 'tags' in tdict and tdict['tags']:
-        head += ' ' + tags(tdict.pop('tags'))
-
-    if 'links' in tdict and tdict['links']:
-        head += ' ' + links(tdict.pop('links'))
-
-    post = '\n'.join([posting(p) for p in tdict.pop('postings')])
-
-    meta = metadata(tdict)
-
-    if meta:
-        return [head + '\n' + meta + '\n' + post + '\n']
-    else:
-        return [head + '\n' + post + '\n']
-
-
-def is_on_schedule(req_date, schedule):
-    req_date = date.fromisoformat(req_date)
-    expire = schedule.get('expire', '').strip()
-
-    if expire and req_date >= date.fromisoformat(expire):
-        return False
-
-    if schedule['type'] == 'intervals since':
-        d0 = date.fromisoformat(schedule['since'])
-        return (True if (req_date - d0).days % schedule['interval'] == 0
-                else False)
-    elif schedule['type'] == 'day of month':
-        return True if req_date.day == schedule['day'] else False
-    elif schedule['type'] == 'end of month':
-        return True if (req_date + timedelta(days=1)).day == 1 else False
-    elif schedule['type'] == 'day of months':
-        return (True if (req_date.month in schedule['months']
-                         and req_date.day == schedule['day'])
-                else False)
-    else:
-        return False
-
-
-def noop(request):
-    """Handlbe dummy directive `noop`, no operation.
-    """
-    return []
-
-
-def txn(request):
-    """Handle directive `txn`.
-    """
-    schedule = request.pop('schedule', None)
-    req_date = request.get('date', date.today().isoformat()).strip()
-    if schedule:
-        if is_on_schedule(req_date, schedule):
-            request['date'] = req_date
+        - `d` can be string or `datetime.date`.
+        """
+        if type(d) is str:
+            d = date.fromisoformat(d.strip())
+        elif type(d) is date:
+            pass
         else:
-            return []
+            raise TypeError(f"Illegal type: {type(d)}.")
 
-    return transaction(request)
+        if self.expire and d >= self.expire:
+            return False
 
+        if self.type == 'intervals since':
+            return (True if (d - self.since).days % self.interval == 0
+                    else False)
+        elif self.type == 'day of month':
+            return True if d.day == self.day else False
+        elif self.type == 'end of month':
+            return True if (d + timedelta(days=1)).day == 1 else False
+        elif self.type == 'day of months':
+            return (True if d.month in self.months and d.day == self.day
+                    else False)
+        else:
+            raise ValueError(f"Unexpected schedule type: {self.type}.")
 
-def balance(request):
-    """Handle directive `balance`.
-    """
-    line = request.pop('date') + ' balance ' + request.pop('account')
-    amount = request.pop('amount')
-    currency = request.pop('currency')
-    p = precision.get(currency, 2)
-    line += f" {amount:,.{p}f} {currency}\n"
-    return [line]
+class BeanPosting(Namespace):
+    def __init__(self, **data):
+        super().__init__()
+        self.account = data.pop('account').strip()
+        self.amount = data.pop('amount', 0)
+        self.currency = data.pop('currency', '').strip()
+        self.cost = data.pop('cost', '').strip()
+        self.price = data.pop('price', '').strip()
+        self.meta = BeanMeta(**data)
 
+    def __str__(self):
+        line = ' ' * conf.posting_indent + self.account
 
-def pad(request):
-    """Handle directive `pad`.
-    """
-    pass
+        if self.amount:
+            p = conf.precision.get(self.currency, 2)
+            amt_str = f"{self.amount:,.{p}f}"
+            try:
+                whole_amount_width = amt_str.index('.')
+            except ValueError:
+                whole_amount_width = len(amt_str)
+            finally:
+                pad = ' ' * max(conf.decimal_align
+                                - conf.posting_indent
+                                - len(self.account)
+                                - whole_amount_width - 1, 2)
+            line += pad + amt_str
 
+            line += ' ' + self.currency if self.currency else ''
+            line += ' ' + self.cost if self.cost else ''
+            line += ' ' + self.price if self.price else ''
 
-def gen_journals(request):
-    """Process directive by calling appropriate handlers.
-    """
-    req = request.copy()
-    _ = req.pop('beancount', None)
-    _ = req.pop('message', None)
+        meta = str(self.meta)
+        line += '\n' + meta if meta else ''
+        return line
 
-    handlers = {'txn': txn,
-                'balance': balance,
-                'pad': pad,
-                'noop': noop}
-    return handlers[req.pop('directive', 'txn')](req)
+class BeanTransaction(Namespace):
+    def __init__(self, **data):
+        super().__init__()
+        self.date = data.pop('date', date.today().isoformat()).strip()
+        self.flag = data.pop('flag', '*').strip()
+        self.payee = data.pop('payee', '').strip()
+        self.narration = data.pop('narration', '').strip()
+        self.tags = format_hashtags(data.pop('tags', ''), prefix='#')
+        self.links = format_hashtags(data.pop('links', ''), prefix='^')
+        self.postings = [BeanPosting(**p) for p in data.pop('postings')]
+        self.schedule = (BeanSchedule(**data.pop('schedule'))
+                         if 'schedule' in data else None)
+        self.meta = BeanMeta(**data)
 
+    def __str__(self):
+        if self.schedule and not self.schedule.check(self.date):
+            return ''
 
-def tree_file(journal):
-    """Decide which file to write a journal entry to under tree method.
-    """
-    d = date.fromisoformat(journal[:10])
-    return os.path.join(f"{d.year:04d}", f"{d.month:02d}.bean")
+        text = self.date + ' ' + self.flag
+        text += ' "' + self.payee + '"' if self.payee else ''
+        text += ' "' + self.narration + '"'
+        text += ' ' + self.tags if self.tags else ''
+        text += ' ' + self.links if self.links else ''
 
+        meta = str(self.meta)
+        text += '\n' + meta if meta else ''
 
-def requests_from_string(text):
-    """Extract a list of requests from given string.
-    """
-    data = json.loads(text)
+        for p in self.postings:
+            text += '\n' + str(p)
+
+        return text + '\n'
+
+a = {
+        'date': '2021-11-17\n',
+        'flag': ' *',
+        'narration': 'Internal transfer',
+        'tags': '&beijing+2008   %###tokYO.2020   ',
+        'links': '^band-21t4',
+        'note': ' Make sure I am trimmed   ',
+        'postings': [
+            {'account': ' Assets:Cash  ',
+             'amount': 123.45,
+             'currency': 'AUD ',
+             'note': '!@#$%^&*()_'
+            },
+            {'account': 'Assets:CBA-Offset'}
+            ]
+        }
+
+request_handlers = {'txn': BeanTransaction,
+                    'open': BeanOpen}
+
+def triage(**kwargs):
+    cls = request_handlers[kwargs.pop('directive', 'txn')]
+    return cls(**kwargs)
+
+def parse_request_text(req_text):
+    data = json.loads(req_text)
+
     if type(data) is dict:
-        return [data]
+        return [triage(**data)]
     elif type(data) is list:
-        return data
+        return [triage(**d) for d in data]
     else:
-        return []
+        raise TypeError(f'Unexpected data type: {type(data)}.')
 
-
-def requests_from_file(filename):
-    """Extract a list of requests from given filename.
-    """
+def parse_request_file(filename):
     with open(filename, 'r') as f:
-        text = f.read()
-    return requests_from_string(text)
-
+        return parse_request_text(f.read())
 
 if not hasattr(sys, 'ps1'):
     # Non-interactive run
-
     if args.files:
         requests = itertools.chain.from_iterable(
-                [requests_from_file(f) for f in args.files]
+                [parse_request_file(f) for f in args.files]
                 )
     else:
-        requests = requests_from_string(sys.stdin.read())
+        requests = parse_request_text(sys.stdin.read())
 
-    journals = itertools.chain.from_iterable(
-            [gen_journals(r) for r in requests]
-            )
+    journals = [str(r) for r in requests]
 
-    if args.verbose == 1:
-        _ = [sys.stdout.write(j + '\n') for j in journals]
-    elif args.verbose == 2:
-        request['beancount'] = journals
-        sys.stdout.write(json.dumps(request, indent=args.json_indent) + '\n')
+    if args.verbose >= 1:
+        _ = [sys.stdout.write(j + '\n') for j in journals if j]
 
     if args.mono:
         fn = args.mono if not args.repo else os.path.join(args.repo, args.mono)
@@ -258,7 +250,8 @@ if not hasattr(sys, 'ps1'):
 
     if args.tree:
         for j in journals:
-            subtree = tree_file(j)
+            d = date.fromisoformat(j[:10])
+            subtree = os.path.join(f"{d.year:04d}", f"{d.month:02d}.bean")
             fn = (os.path.join(args.tree, subtree) if not args.repo
                   else os.path.join(args.repo, args.tree, subtree))
             pathlib.Path(os.path.dirname(fn)).mkdir(parents=True,
@@ -270,9 +263,9 @@ if not hasattr(sys, 'ps1'):
                                 os.path.join(args.tree, subtree)])
 
     if args.repo:
-        message = request.get('message', args.message)
         if args.commit:
-            subprocess.run(['git', '-C', args.repo, 'commit', '-m', message])
+            subprocess.run(['git', '-C', args.repo,
+                            'commit', '-m', args.message])
         if args.push:
             subprocess.run(['git', '-C', args.repo, 'push'])
 
